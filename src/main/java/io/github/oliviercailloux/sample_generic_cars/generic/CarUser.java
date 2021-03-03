03e041f6de6662e6eda61e9a8e2f01ece00d291d
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2021-03-03 16:38:28 +0100

    Usage of generic method

--- src/main/java/io/github/oliviercailloux/sample_generic_cars/generic/CarUser.java
+++ src/main/java/io/github/oliviercailloux/sample_generic_cars/generic/CarUser.java
@@ -6,6 +6,15 @@ public class CarUser {
 		Car<Integer> car2 = new Car<>(3);
 		System.out.println(car1.toString());
 		System.out.println(car2.toString());
+
+		String stringEngineCar1 = CarUser.getEngineAsString(car1);
+		System.out.println(stringEngineCar1);
+		/*
+		 * We may specify explicitly the type E, as follows, but it is not necessary
+		 * (and not recommended) as the compiler can deduce it for us.
+		 */
+		String stringEngineCar2 = CarUser.<Integer>getEngineAsString(car2);
+		System.out.println(stringEngineCar2);
 	}
 
 	public static <E> String getEngineAsString(Car<E> car) {
